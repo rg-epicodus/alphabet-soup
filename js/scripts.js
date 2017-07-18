79170ba9d1bf7dfc8ed39ccbeffa6eb15c8a884f
Rich Garrick <richg341@gmail.com>
2017-07-17 20:40:06 -0700

    add audio sound of first letter on click.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
+  $("#playButton").click(function(playSound) {
+    // event.preventDefault;
 
-var soundFile5 = 'audio/arabic/L_05.mp3';
-playSound(soundFile);
-setInterval(function () {
+    var soundFile = 'audio/arabic/L_05.mp3';
     playSound(soundFile);
-}, 1000);
+    // setInterval(function () {
+    //     playSound(soundFile);
+    // }, 4000);
 
-function playSound(audio) {
-    var soundElement = '<audio style="display:none; width: 0px; height: 0px;" id="audioNotifier" src="' + audio + '" controls preload="auto" autobuffer></audio>';
-    $('#audioContainer').html(soundElement);
-    $('#audioNotifier')[0].play();
-}
+    function playSound(audio) {
+        var soundElement = '<audio style="display:none; width: 0px; height: 0px;" id="audioNotifier" src="' + audio + '" controls preload="auto" autobuffer></audio>';
+        $('#audioContainer').html(soundElement);
+        $('#audioNotifier')[0].play();
+    }
 
+  });
 });
